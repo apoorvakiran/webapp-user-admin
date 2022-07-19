e17be2a1d9e0a9b62f514674d21b4e0d9b1276eb
Ravish Arya <ravish.arya@trigyn.com>
2022-07-19 17:15:56 +0530

    mentore cognito changes

--- src/features/Configuration/config.js
+++ src/features/Configuration/config.js
@@ -1,8 +1,12 @@
 const config = {
+    // Auth: {
+    //     'userPoolId': 'us-east-1_A3GGYC1RB',
+    //     'userPoolWebClientId': '1f3qolcmsljqelp0lcu942sqvo',
+    // } // mindy
     Auth: {
-        'userPoolId': 'us-east-1_A3GGYC1RB',
-        'userPoolWebClientId': '1f3qolcmsljqelp0lcu942sqvo',
-    }
+        'userPoolId': 'us-east-2_vahTpJjVJ',
+        'userPoolWebClientId': '7mq0vkr4hls2ctqfus8lm2psl',
+    } //mentore
 }
 
 export default config;
