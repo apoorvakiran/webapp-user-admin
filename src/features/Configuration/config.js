f042f5fea18ac027640946f64240835632aab973
Ravish Arya <ravish.arya@trigyn.com>
2022-08-17 12:09:02 +0530

    rivian env changes

--- src/features/Configuration/config.js
+++ src/features/Configuration/config.js
@@ -4,9 +4,9 @@ const config = {
     //     'userPoolWebClientId': '1f3qolcmsljqelp0lcu942sqvo',
     // } // mindy
     Auth: {
-        'userPoolId': 'us-east-2_WsbmacZjZ',
-        'userPoolWebClientId': '1498c3m2balflj7hhvde5sakkp',
-    } //mentore
+        'userPoolId': 'us-west-2_cryLCboir',
+        'userPoolWebClientId': '2h3hla6rcb6ojmmncjlugn66k2',
+    } //rivian
 }
 
 export default config;
