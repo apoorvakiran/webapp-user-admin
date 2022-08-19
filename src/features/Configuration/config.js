c9439de2da540f85cbf5a389b39e941fa34b6f3b
Ravish Arya <ravish.arya@trigyn.com>
2022-08-19 17:53:03 +0530

    env config automation

--- src/features/Configuration/config.js
+++ src/features/Configuration/config.js
@@ -1,12 +1,8 @@
 const config = {
-    // Auth: {
-    //     'userPoolId': 'us-east-1_A3GGYC1RB',
-    //     'userPoolWebClientId': '1f3qolcmsljqelp0lcu942sqvo',
-    // } // mindy
     Auth: {
-        'userPoolId': 'us-west-2_cryLCboir',
-        'userPoolWebClientId': '2h3hla6rcb6ojmmncjlugn66k2',
-    } //rivian
+        'userPoolId': process.env.REACT_APP_USER_POOL,
+        'userPoolWebClientId': process.env.REACT_APP_WEB_CLIENT,
+    }
 }
 
 export default config;
