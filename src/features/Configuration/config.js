b356494a11d6bf7e3ffadcb37d3d704acb42efa4
Yogesh Sarmalkar <yogesh.sarmalkar@trigyn.com>
2022-09-13 12:09:48 +0530

    auto logo image display from s3

--- src/features/Configuration/config.js
+++ src/features/Configuration/config.js
@@ -2,6 +2,14 @@ const config = {
     Auth: {
         'userPoolId': process.env.REACT_APP_USER_POOL,
         'userPoolWebClientId': process.env.REACT_APP_WEB_CLIENT,
+        'identityPoolId': 'us-east-2:7e918941-abdd-4be2-bb0d-e34df35f9ffc',
+        'region': 'us-east-2',
+    },
+    Storage: {
+        AWSS3: {
+            'bucket': 'mentore-tenant-images', //REQUIRED -  Amazon S3 bucket name
+            'region': 'us-east-2', //OPTIONAL -  Amazon service region
+        }
     }
 }
 
