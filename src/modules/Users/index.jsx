41f7e2d42a49fb000eca962fcb64de72dac1789c
Yogesh Sarmalkar <yogesh.sarmalkar@trigyn.com>
2022-10-04 18:09:34 +0530

    changes for enhanced search and sort

--- src/modules/Users/index.jsx
+++ src/modules/Users/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Button, Card, Radio, Skeleton } from "antd";
+import { Button, Card, Input, Radio, Skeleton } from "antd";
 import Table from "../../components/Table/index";
 import { PlusOutlined, EditOutlined } from "@ant-design/icons";
 import { editUserButton, cardStyle } from "./style";
@@ -20,7 +20,7 @@ const Users = props => {
   const [dataSource, setDataSource] = useState([]);
   const [width, setWidth] = useState(0)
   const [defaultJob, setDefaultJob] = useState({})
-
+  const [searchedText, setSearchedText] = useState("");
 
   const userRole = useContext(UserRoleContext);
 
@@ -28,27 +28,27 @@ const Users = props => {
     const { innerWidth: width, innerHeight: height } = window;
     setWidth(width);
   };
-  
+
   async function getJobTitleList() {
     const jobList = await usersJobsList()
     setDefaultJob(jobList);
   }
 
   useEffect(() => {
-  async function getData() {
-    const idToken = await getAuthData();
-    const response = await axios.get(
-      baseUrl + "user", {
-      // "http://localhost:5051/api/user-admin/user-list"
-      headers: {
-        "Authorization": `Bearer ${idToken}`
-      },
-      params: { type: "user-list" }
+    async function getData() {
+      const idToken = await getAuthData();
+      const response = await axios.get(
+        baseUrl + "user", {
+        // "http://localhost:5051/api/user-admin/user-list"
+        headers: {
+          "Authorization": `Bearer ${idToken}`
+        },
+        params: { type: "user-list" }
+      }
+      );
+      setLoading(false);
+      setDataSource(response.data.data);
     }
-    );
-    setLoading(false);
-    setDataSource(response.data.data);
-  }
     getData();
     getWindowDimensions();
     getJobTitleList()
@@ -83,7 +83,7 @@ const Users = props => {
       </Button>
     );
   };
-  
+
   const onChange = (e, record) => {
     const newRecord = {
       ...record,
@@ -101,6 +101,17 @@ const Users = props => {
       sorter: (a, b) => a.first_name.localeCompare(b.first_name),
       fixed: "left",
       width: "10rem",
+      filteredValue: [searchedText],
+      onFilter: (value, record) => {
+        return String(record.first_name).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.role).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.name).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.email).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.phone).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.id_number).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.mac).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.hand).toLowerCase().includes(value.toLowerCase());
+      },
       render(item, record) {
         return {
           props: {
@@ -232,6 +243,10 @@ const Users = props => {
       key: "first_name",
       sorter: true,
       width: "10rem",
+      filteredValue: [searchedText],
+      onFilter: (value, record) => {
+        return String(record.first_name).toLowerCase().includes(value.toLowerCase());
+      },
       render(item, record) {
         return {
           props: {
@@ -297,6 +312,16 @@ const Users = props => {
             </Button>
           </div>
           <div className="usersTable">
+            <Input.Search
+              placeholder="Search here...."
+              style={{ marginBottom: 8 }}
+              onSearch={(value) => {
+                setSearchedText(value);
+              }}
+              onChange={(e) => {
+                setSearchedText(e.target.value);
+              }}
+            />
             <Table
               data={dataSource}
               columns={parseInt(width) > 480 ? columns : mobileColumns}
