8eeb419cee7131d3eca19f4c38ef09f7a03296c8
Yogesh Sarmalkar <yogesh.sarmalkar@trigyn.com>
2022-10-20 17:52:14 +0530

    changes to remove capping values text in description

--- src/utils/Data/Data.js
+++ src/utils/Data/Data.js
@@ -128,7 +128,7 @@ export const UserRole = '2';
 export const ActiveScoreDesc = "A metric of productivity measured by the ratio measured in percentage of intense active motion vs mild active motion. Value ranges from 0% to 100%. It is an indicator of individual productivity and engagement.";
 export const SafetyScoreDesc = "Measures the risk of injury due to poor ergonomic motion. Value ranges from 0 to 7. The higher the number, the higher the risk of injury. The dominant motion (pitch, yaw, roll) used in this index is speed of pitch. It is a measure of force on the hand and wrist.";
 export const SpeedScoreDesc = "Measures the speed (and force) on the hand and wrist. Value ranges from 0 to 7. The higher the number, the higher the force. The speed score takes into account maximum values among pitch, yaw and roll for each sample. It is a measure of how fast the hand and wrist are moving.";
-export const RiskScoreDesc = "A count of the number of times the Risk Index exceeds a safe value. Value ranges from 0 to 10. The higher the number, the higher the risk of injury. The maximum value is capped at 10.";
+export const RiskScoreDesc = "A count of the number of times the Risk Index exceeds a safe value. The higher the number, the higher the risk of injury.";
 
 export const getDescription = activeTab => {
     switch (activeTab) {
@@ -137,7 +137,7 @@ export const getDescription = activeTab => {
         case "Injury Risk Score":
             return "Measures the risk of injury due to poor ergonomic motion. Value ranges from 0 to 7. The higher the number, the higher the risk of injury. The dominant motion (pitch, yaw, roll) used in this index is speed of pitch. It is a measure of force on the hand and wrist.";
         case "Risk Frequency":
-            return "A count of the number of times the Risk Index exceeds a safe value. Value ranges from 0 to 10. The higher the number, the higher the risk of injury. The maximum value is capped at 10.";
+            return "A count of the number of times the Risk Index exceeds a safe value. The higher the number, the higher the risk of injury.";
         case "Speed Score":
             return "Measures the speed (and force) on the hand and wrist. Value ranges from 0 to 7. The higher the number, the higher the force. The speed score takes into account maximum values among pitch, yaw and roll for each sample. It is a measure of how fast the hand and wrist are moving. ";
         default:
