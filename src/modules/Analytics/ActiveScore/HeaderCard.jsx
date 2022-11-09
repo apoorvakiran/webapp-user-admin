2656046675c7839c58521f069917b936533cbcdc
Yogesh Sarmalkar <yogesh.sarmalkar@trigyn.com>
2022-11-09 12:49:27 +0530

    TM-129 mobile view UI issue fix

--- src/modules/Analytics/ActiveScore/HeaderCard.jsx
+++ src/modules/Analytics/ActiveScore/HeaderCard.jsx
@@ -8,20 +8,21 @@ const HeaderCard = props => {
 
     return (
         <div className="speed-score-container">
-            <div style={{ width: "33%" }}>
+            <div className="dropdownWidth">
                 <Select
                     defaultValue="high"
                     onChange={handleChange}
-                    style={{ width: 120 }}
+                    style={{ width: "auto" }}
                 >
                     <Option value="high">Sort: High</Option>
                     <Option value="low" data-testid="low">Sort: Low</Option>
                 </Select>
             </div>
+            <div className="spaceDiv"></div>
             <div className="speed-score-average">
-                <Typography style={{ width: "33%", color: "#00CF15", fontWeight: "700" }} data-testid="minValue">{props.minValue}</Typography>
+                <Typography className="rangeLabelStart" data-testid="minValue">{props.minValue}</Typography>
                 {/*<Typography style={{ width: "34%", textAlign: "center", color: "#F3AF00", fontWeight: "700" }}>{"Average"}</Typography>*/}
-                <Typography style={{ width: "33%", textAlign: "end", color: "#D10000", fontWeight: "700" }} data-testid="maxValue">{props.maxValue}</Typography>
+                <Typography className="rangeLabelEnd" data-testid="maxValue">{props.maxValue}</Typography>
             </div>
         </div>
     );
